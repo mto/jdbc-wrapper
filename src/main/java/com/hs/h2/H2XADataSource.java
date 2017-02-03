edd0892d9d832ab7ebad0fb35b6bc818fc9e6b5b
Minh Hoang TO <hoang281283@gmail.com>
2017-02-03 19:06:42 +0700

    Implement factory for H2XADataSource

--- src/main/java/com/hs/h2/H2XADataSource.java
+++ src/main/java/com/hs/h2/H2XADataSource.java
@@ -1,17 +1,18 @@
 package com.hs.h2;
 
 import com.hs.sql.XADataSourceWrapper;
-import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.XADataSource;
 
 /**
+ * This class is necessary for JNDI configuration
+ *
  * @author <a href="hoang281283@gmail.com">Minh Hoang TO</a>
  * @date: 2/3/17
  */
 public class H2XADataSource extends XADataSourceWrapper {
 
-    public H2XADataSource(JdbcDataSource h2ds){
-        super(h2ds);
+    public H2XADataSource(XADataSource target){
+        super(target);
     }
 }
--- src/main/java/com/hs/h2/H2XADataSourceFactory.java
+++ src/main/java/com/hs/h2/H2XADataSourceFactory.java
@@ -1,7 +1,12 @@
 package com.hs.h2;
 
+import com.hs.md.HSConfig;
+import com.hs.md.Profile;
+import org.h2.jdbcx.JdbcDataSource;
+
 import javax.naming.Context;
 import javax.naming.Name;
+import javax.naming.Reference;
 import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
@@ -11,8 +16,50 @@ import java.util.Hashtable;
  */
 public class H2XADataSourceFactory implements ObjectFactory {
 
+    static {
+        org.h2.Driver.load();
+    }
+
+    /**
+     * Fork the code of org.h2.jdbcx.JdbcDataSourceFactory and intercept the URL
+     */
     @Override
-    public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+    public synchronized Object getObjectInstance(Object obj, Name name,
+                                                 Context nameCtx, Hashtable<?, ?> environment) {
+        if (obj instanceof Reference) {
+            Reference ref = (Reference) obj;
+            if (ref.getClassName().equals(H2XADataSource.class.getName())) {
+
+                String configURL = (String) ref.get("url").getContent();
+                String jdbcURL = configURL;
+                Profile prof = null;
+
+                if (configURL.startsWith("jdbc:hs:")) {
+                    try {
+                        String pname = configURL.substring("jdbc:hs:".length());
+                        prof = HSConfig.getInstance().getProfile(pname);
+
+                        jdbcURL = prof.getJDBCParam(HSConfig.JDBC_URL_PARAM_NAME);
+                    } catch (Exception ex) {
+                        //TODO: Add log
+                    }
+                }
+
+                JdbcDataSource dataSource = new JdbcDataSource();
+                dataSource.setURL(jdbcURL);
+                dataSource.setUser((String) ref.get("user").getContent());
+                dataSource.setPassword((String) ref.get("password").getContent());
+                dataSource.setDescription((String) ref.get("description").getContent());
+                String s = (String) ref.get("loginTimeout").getContent();
+                dataSource.setLoginTimeout(Integer.parseInt(s));
+
+                H2XADataSource h2xaobj = new H2XADataSource(dataSource);
+                h2xaobj.injectProfile(prof);
+
+                return h2xaobj;
+            }
+        }
         return null;
     }
+
 }
